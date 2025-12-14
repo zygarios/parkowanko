c2a1ae048a7f033ca34e42c49f78e544f428410c
Bartosz Zygmunt <bartekzygmunt92@gmail.com>
2025-12-14 23:51:22 +0100

    add pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
+import { provideServiceWorker } from '@angular/service-worker';
 import { provideSentry } from './_others/_helpers/sentry';
-import { httpInterceptor } from './_others/_interceptors/http.interceptor';
 import { authInterceptor } from './_others/_interceptors/auth.interceptor';
+import { httpInterceptor } from './_others/_interceptors/http.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -20,6 +21,10 @@ export const appConfig: ApplicationConfig = {
       useValue: { appearance: 'outline' },
     },
     { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 3000 } },
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: `registerWhenStable:${30000}`,
+    }),
     // {
     //   provide: STEPPER_GLOBAL_OPTIONS,
     //   useValue: { displayDefaultIndicatorType: false },
